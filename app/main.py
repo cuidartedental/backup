93887474690c2f0bbbdcb4b63a632f7f8e6ff0cf
cuidartedental <cuidartedental78@gmail.com>
2025-12-29 15:50:40 -0400

    agregated cors

--- app/main.py
+++ app/main.py
@@ -9,13 +9,16 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0")
 
 # Configuración de CORS
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://cuidarte-backup.netlify.app",
+]
+
+# 2. El middleware debe estar configurado ASÍ:
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://cuidarte-backup.netlify.app/",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
