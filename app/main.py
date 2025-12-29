66e0a931ce22522fcced4f3819c156c2cda2ab29
cuidartedental <cuidartedental78@gmail.com>
2025-12-29 15:56:48 -0400

    agregated method part2

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
-# app/main.py
 from fastapi import FastAPI
 from app.routes import upload
 from app.config.settings import settings
-
 from fastapi.middleware.cors import CORSMiddleware
 
-# app = FastAPI(title="Dental Backup API")
-app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0")
+# Desactivamos el redireccionamiento automático de slashes para evitar conflictos de CORS
+app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0", redirect_slashes=False)
 
-# Configuración de CORS
+# Configuración de CORS extendida
 origins = [
     "https://cuidarte-backup.netlify.app",
-    "https://cuidarte-backup.netlify.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
@@ -20,11 +17,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_methods=["*"],  # Cambiado a "*" para asegurar que OPTIONS pase siempre
     allow_headers=["*"],
     expose_headers=["*"],
 )
-
 
 # Incluir rutas
 app.include_router(upload.router, prefix="/api/v1", tags=["Upload"])
